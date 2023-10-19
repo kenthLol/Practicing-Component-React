e0d4e8aa161431da4c29a74d013b218354260758
Kenneth Lola <kenthsamu@gmail.com>
2023-10-19 12:48:42 -0600

    feat: Add to favorites feature added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import BookList from "./components/ListBooks";
 import Navbar from "./components/Navbar";
+import ListFavorites from "./components/ListFavorites";
+import dataBook from "./data/books";
 import "./App.css";
-// import logo from "./logo.svg";
+
+import { useState } from "react";
 
 function App() {
+  function addBookToFavorite(element) {
+    console.log(element);
+    let tempBookList = [...ListFavoriteBooks];
+    tempBookList.push(element);
+    setListFavoriteBooks(tempBookList);
+  }
+
+  function removeBookFromFavorites(element) {
+    const updatedFavorites = ListFavoriteBooks.filter(
+      (book) => book.id !== element.id
+    );
+    setListFavoriteBooks(updatedFavorites);
+  }
+
+  const [bookList, setBookList] = useState(dataBook);
+  const [ListFavoriteBooks, setListFavoriteBooks] = useState([]);
+
   return (
     <div className="App">
       <Navbar />
-      <BookList />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-9">
+            <BookList elements={bookList} fnAddFavorites={addBookToFavorite} />
+          </div>
+          <div className="col-md-3">
+            <ListFavorites
+              elements={ListFavoriteBooks}
+              fnRemoveBookFromFavorites={removeBookFromFavorites}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
